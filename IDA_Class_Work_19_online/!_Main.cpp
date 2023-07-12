a0f5c8db6a058e15c2a9dd2ddafa3f49643338f1
Ишутинов Данил <gladius@inbox.ru>
2023-07-12 18:35:04 +0500

    Add project files.

---
+++ IDA_Class_Work_19_online/!_Main.cpp
@@ -0,0 +1,51 @@
+﻿//class template version 5.0 //07.07.2023//
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <time.h>
+#include <algorithm>
+#include <conio.h>
+//My own headers
+#include "Service functions.h"
+#include "Task_1.h"
+
+
+// Class Work 12.07.2023 online -------------------------------------------	
+
+//Task 1
+void Task_1()
+{
+
+}
+//Task 2
+void Task_2()
+{
+	
+}
+//Task 3
+void Task_3()
+{
+
+}
+
+
+// MAIN ------- MAIN ------- MAIN ------- MAIN ------- MAIN --------
+int main()
+{
+srand(time(NULL));
+		
+	
+Task_1();
+Task_2();
+Task_3();
+
+
+
+
+
+std::cout << "\n\n";
+//system("pause");	
+_getch();
+}
+
+
